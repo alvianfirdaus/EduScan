f71bfc2e05c02c300812f89452c2f73a95773ed9
Zahra Annisa Wahono <zahraannisawahono08@gmail.com>
2023-11-04 21:27:06 +0700

    Implementasi Plugin Camera

--- lib/pages/scan.dart
+++ lib/pages/scan.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/routes/route.dart';
 import 'package:myapp/widgets/cekrik.dart';
@@ -9,63 +10,121 @@ class ScanState extends StatefulWidget {
 }
 
 class _ScanState extends State<ScanState> {
+  List<CameraDescription> cameras = [];
+
+  Future<Widget> initializeAndDisplayCamera() async {
+    try {
+      // Menginisialisasi CameraController
+      final cameraController = CameraController(
+        cameras[0],
+        ResolutionPreset.medium,
+      );
+
+      // Menginisialisasi controller dan menunggu inisialisasi selesai
+      await cameraController.initialize();
+
+      // Menampilkan kamera
+      return CameraPreview(cameraController);
+    } catch (e) {
+      print("Kesalahan saat mengakses kamera: $e");
+      // Lakukan tindakan yang sesuai jika terjadi kesalahan
+      return Text("Tidak dapat mengakses kamera: $e");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Inisialisasi kamera
+    availableCameras().then((value) {
+      setState(() {
+        cameras = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Text(
+            'Scan',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          centerTitle: true,
+          backgroundColor: Color(0xff03a1fe),
+          automaticallyImplyLeading: false,
+        ),
         floatingActionButton: CekrikButtonBar(),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         bottomNavigationBar: BottomApp(),
         body: Stack(
           children: [
-            Container(
-              width: 800,
-              height: 900,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(0),
-                image: DecorationImage(
-                  image:
-                      AssetImage('assets/images/pexels-photo-by-alex-garcia.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: cameras.isEmpty
+                  ? Container(
+                      color: Color(1),
+                    )
+                  : FutureBuilder<Widget>(
+                      future: initializeAndDisplayCamera(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          return snapshot.data ??
+                              Container(color: Colors.green);
+                        } else if (snapshot.hasError) {
+                          return Text("Kesalahan: ${snapshot.error}");
+                        } else {
+                          return CircularProgressIndicator();
+                        }
+                      },
+                    ),
             ),
-            Container(
-              width: 950,
-              height: 800,
-              child: Image.asset('assets/images/image-2.png', scale: 1.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AspectRatio(
+                  aspectRatio: 1,
+                  child: Column(
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Container(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: AspectRatio(
+                          aspectRatio: (1.56 / 1),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(),
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(
-                    context, Routes.riwayat); // Navigasi ke halaman "Sign In"
-              },
-              child: Container(
-                child: Text(
-                  'Scan',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-                height: 60,
-                width: 1500,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.white54,
-                    width: 1,
-                  ),
-                  image: DecorationImage(
-                    image: AssetImage(
-                        'assets/images/iconly-regular-outline-arrow-left.png'),
-                    alignment: Alignment.centerLeft,
-                  ),
-                  borderRadius: BorderRadius.circular(0),
-                  color: Colors.grey[200],
-                ),
-                padding: EdgeInsets.only(top: 10, bottom: 8),
-                alignment: Alignment.topCenter,
-              ),
-            ),
+            
           ],
         ),
       ),
