d07c557166467a7e16521834887e75f79885da6a
= <alvianfirdaus896@gmail.com>
2023-12-10 23:12:12 +0700

    W15: add SingleChildScrollView di Sign Up

--- lib/pages/page_sign_up.dart
+++ lib/pages/page_sign_up.dart
@@ -13,7 +13,7 @@ class pageSignUp extends StatefulWidget {
 
   @override
   State<pageSignUp> createState() => _pageSignUpState();
-  }
+}
 
 class _pageSignUpState extends State<pageSignUp> {
   final _formKey = GlobalKey<FormState>();
@@ -23,7 +23,6 @@ class _pageSignUpState extends State<pageSignUp> {
   var enteredPasswordRepeat = '';
 
   void _submit() async {
-    
     final isValid = _formKey.currentState!.validate();
     if (!isValid) {
       return;
@@ -31,8 +30,7 @@ class _pageSignUpState extends State<pageSignUp> {
     _formKey.currentState!.save();
     try {
       final userCredentials =
-          await 
-          _firebaseAuth.createUserWithEmailAndPassword(
+          await _firebaseAuth.createUserWithEmailAndPassword(
               email: enteredEmail, password: enteredPassword);
       await userCredentials.user!.updateDisplayName(enteredName);
 
@@ -46,7 +44,8 @@ class _pageSignUpState extends State<pageSignUp> {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text(message),
-          backgroundColor: Theme.of(context).errorColor,),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
       );
     } catch (error) {
       print(error);
@@ -57,140 +56,144 @@ class _pageSignUpState extends State<pageSignUp> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Form(
-          key: _formKey,
-          child: Center(
-            child: Column(children: [
-              Container(
-                  padding: EdgeInsets.symmetric(vertical: 50),
-                  child: const Image(
-                    image: AssetImage('assets/images/logoEduscan.png'),
-                    width: 100,
-                    height: 100,
-                  )),
-              Container(
-                padding: EdgeInsets.only(bottom: 30),
-                child: CustomTextStyle(
-                  text: 'Selamat Datang di EduScan',
-                  fontWeight: FontWeight.w600,
-                  fontSize: 23,
+        resizeToAvoidBottomInset: true,
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Center(
+              child: Column(children: [
+                Container(
+                    padding: EdgeInsets.symmetric(vertical: 50),
+                    child: const Image(
+                      image: AssetImage('assets/images/logoEduscan.png'),
+                      width: 100,
+                      height: 100,
+                    )),
+                Container(
+                  padding: EdgeInsets.only(bottom: 30),
+                  child: CustomTextStyle(
+                    text: 'Selamat Datang di EduScan',
+                    fontWeight: FontWeight.w600,
+                    fontSize: 23,
+                  ),
                 ),
-              ),
-              Container(
-                child: CustomTextStyle(text: 'Buat akun EduScan anda.'),
-              ),
-              Container(
-                child: customTextField(
-                  label: 'Nama Lengkap',
-                  hint: 'Masukkan Nama Lengkap',
-                  textInputType: TextInputType.name,
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Nama tidak boleh kosong';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    enteredName = value!;
-                  },
+                Container(
+                  child: CustomTextStyle(text: 'Buat akun EduScan anda.'),
                 ),
-              ),
-              Container(
-                child: customTextField(
-                    label: 'Email',
-                    hint: 'Masukkan Email',
-                    textInputType: TextInputType.emailAddress,
+                Container(
+                  child: customTextField(
+                    label: 'Nama Lengkap',
+                    hint: 'Masukkan Nama Lengkap',
+                    textInputType: TextInputType.name,
                     validator: (value) {
-                      if (value.isEmpty || !value.contains('@')) {
-                        return 'Email tidak tidak valid';
+                      if (value.isEmpty) {
+                        return 'Nama tidak boleh kosong';
                       }
                       return null;
                     },
                     onSaved: (value) {
-                      enteredEmail = value!;
-                    }),
-              ),
-              Container(
-                child: customTextField(
-                  label: 'Password',
-                  hint: 'Masukkan Password',
-                  textInputType: TextInputType.visiblePassword,
-                  hiddenText: true,
-                  validator: (value) {
-                    if (value.isEmpty || value.length < 8) {
-                      return 'Password kurang dari 8 karakter';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    enteredPassword = value!;
-                  },
+                      enteredName = value!;
+                    },
+                  ),
                 ),
-              ),
-              Container(
-                child: customTextField(
-                    label: 'Ulangi Password',
-                    hint: 'Masukkan Ulang Password',
-                    textInputType: TextInputType.visiblePassword, hiddenText: true,
+                Container(
+                  child: customTextField(
+                      label: 'Email',
+                      hint: 'Masukkan Email',
+                      textInputType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value.isEmpty || !value.contains('@')) {
+                          return 'Email tidak tidak valid';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        enteredEmail = value!;
+                      }),
+                ),
+                Container(
+                  child: customTextField(
+                    label: 'Password',
+                    hint: 'Masukkan Password',
+                    textInputType: TextInputType.visiblePassword,
+                    hiddenText: true,
                     validator: (value) {
                       if (value.isEmpty || value.length < 8) {
                         return 'Password kurang dari 8 karakter';
-                        //} else if (value != enteredPassword) {
-                        //  return 'Password tidak sama';
                       }
                       return null;
                     },
                     onSaved: (value) {
-                      enteredPasswordRepeat = value!;
-                    }),
-              ),
-              ButtonBar(
-                alignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {
-                      _submit();
-                       //Navigator.pushNamed(context, Routes.signIn);
+                      enteredPassword = value!;
                     },
-                    style: ButtonStyle(
-                      minimumSize: MaterialStatePropertyAll(Size(300, 50)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                      ),
-                    ),
-                    child: Text(
-                      'Daftar',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
                   ),
-                  ],
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, Routes.signIn);
-                },
-                child: Container(
-                  padding: EdgeInsets.all(16),
-                  child: Text.rich(
-                    TextSpan(
-                      text: 'Sudah memiliki akun? ',
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: 'Masuk',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                ),
+                Container(
+                  child: customTextField(
+                      label: 'Ulangi Password',
+                      hint: 'Masukkan Ulang Password',
+                      textInputType: TextInputType.visiblePassword,
+                      hiddenText: true,
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 8) {
+                          return 'Password kurang dari 8 karakter';
+                          //} else if (value != enteredPassword) {
+                          //  return 'Password tidak sama';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        enteredPasswordRepeat = value!;
+                      }),
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        _submit();
+                        //Navigator.pushNamed(context, Routes.signIn);
+                      },
+                      style: ButtonStyle(
+                        minimumSize: MaterialStatePropertyAll(Size(300, 50)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
                           ),
                         ),
-                      ],
+                      ),
+                      child: Text(
+                        'Daftar',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, Routes.signIn);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    child: Text.rich(
+                      TextSpan(
+                        text: 'Sudah memiliki akun? ',
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Masuk',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ]),
+              ]),
+            ),
           ),
         ),
       ),
